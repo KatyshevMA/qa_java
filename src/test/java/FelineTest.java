973f0a9ee22a57644f228c2e26ecfbfcd468523f
Mikhail Katyshev <katyshevma@yandex.ru>
2023-05-17 20:59:04 +0300

    Jacoco 100% v2

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -2,11 +2,9 @@ import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
-import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
